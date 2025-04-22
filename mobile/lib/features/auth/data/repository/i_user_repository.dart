047620d4d22855a77108de492c22ebce6ba0d82c
Mido2610 <kiet0933362664@gmail.com>
2025-04-22 15:38:04 +0700

    fix: fix proto

--- mobile/lib/features/auth/data/repository/i_user_repository.dart
+++ mobile/lib/features/auth/data/repository/i_user_repository.dart
@@ -2,7 +2,7 @@ import 'package:dating_app/core/utils/common.dart';
 import 'package:dating_app/features/auth/data/client/end_point.dart';
 import 'package:dating_app/features/auth/data/client/https_client.dart';
 import 'package:dating_app/features/auth/data/repository/user_repository.dart';
-import 'package:dating_app/proto/gen/user.pb.dart';
+import 'package:dating_app/proto/gen/auth.pb.dart';
 
 class IUserRepository implements UserRepository {
   final HttpsClient _httpsClient;
